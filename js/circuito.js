59e84795a594b452a1feee7b328a16e7f719808f
Natalia Blanco <UO295340@uniovi.es>
2025-11-22 13:04:31 +0100

    Ejercicio 3 Práctica 9 completado: Añadido el mapa dinámico para importar el `circuito.kml`

--- js/circuito.js
+++ js/circuito.js
@@ -156,6 +156,111 @@ class CargadorSVG {
 
         contenedor.appendChild(svgElemento);
     }
+}
 
+// Clase para la visualizacion de mapas
+class CargadorKML {
+    
+    #mapa;
+    #coordenadas;
+    #origen
 
+    constructor() {
+        const contenedorMapa = document.querySelector("div");
+        mapboxgl.accessToken = 'pk.eyJ1IjoibmF0YWxpYTA0IiwiYSI6ImNtM3E4MnFsNDBsMDAyanNhcmtxbzB5ZWEifQ.6cgByNf8BnyghepPNtdpMQ'
+
+        this.#mapa = new mapboxgl.Map({
+        container: contenedorMapa,
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [-3.0, 43.0],
+        zoom: 5
+    });
+        this.#coordenadas = [];
+        this.#origen = null;
+
+        this.#inicializarEventos();
+    }
+
+    /**
+     * Inicializa el evento del botón
+     */
+    #inicializarEventos() {
+        const inputArchivo = document.querySelectorAll('input[type="file"]')[2];
+        inputArchivo.addEventListener('change', (evento) => {
+            const archivoSeleccionado = evento.target.files[0];
+            if (archivoSeleccionado) {
+                this.#leerArchivoKML(archivoSeleccionado);
+            }
+        })
+    }
+
+    // Carga un archivo KML desde la máquina cliente utilizando API file
+    #leerArchivoKML(archivo){
+        const lector = new FileReader();
+
+        lector.onload = () => {
+            const texto = lector.result;
+            this.#procesarKML(texto);
+            this.#insertarCapaKML();
+        };
+
+        lector.onerror = () => {
+            console.log("Error leyendo el archivo");
+        }
+
+        lector.readAsText(archivo, "UTF-8");
+    }
+
+    // Procesa el kml pasado como parametro
+    #procesarKML(kmlText){
+        const parser = new DOMParser();
+        const xml = parser.parseFromString(kmlText, "text/xml");
+
+        const coords = xml.getElementsByTagName("coordinates")[0].textContent.trim();
+        const puntos = coords.split(/\s+/);
+
+        this.#coordenadas = puntos.map(p => {
+            const [lon, lat] = p.split(",").map(Number);
+            return [lon, lat];
+        });
+
+        this.#origen = this.#coordenadas[0];
+    }
+
+    // Superpone un archivo KML en un mapa
+    #insertarCapaKML(){
+        if(!this.#origen || this.#coordenadas.length === 0) return;
+
+        this.#mapa.flyTo({
+            center: this.#origen,
+            zoom: 13.5,
+            speed: 1.2,
+            curve: 1.5,
+            essential: true
+        });
+
+        // Marcador del inicio del circuito
+        new mapboxgl.Marker().setLngLat(this.#origen).addTo(this.#mapa);
+
+        // Linea del circuito
+        this.#mapa.addSource('circuito', {
+            'type': 'geojson',
+            'data': {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'LineString',
+                    'coordinates': this.#coordenadas
+                }
+            }
+        });
+
+        this.#mapa.addLayer({
+            'id': 'circuito-linea',
+            'type': 'line',
+            'source': 'circuito',
+            'paint': {
+                'line-width': 4
+            }
+        })
+    }
 }
